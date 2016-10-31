03cf987c1f33a3f31432aa75bf4ef05b10ab5b68
Jimmy <odylic.lin@gmail.com>
2016-10-31 12:42:25 -0400

    3rd

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ var backgroundv;
 
 var player;
 
+var cursors;
+
+var bullets;
+var bulletTime = 0;
+var fireButton;
+
 
 
 var mainState = {
@@ -14,6 +20,7 @@ var mainState = {
 		game.load.image('starfield', "assets/starfield.png");
 		game.load.image('player', "assets/Chrono.gif");
 
+		game.load.image('bullet', "assets/bullet.png");
 
 	},
 
@@ -22,19 +29,65 @@ var mainState = {
 		spacefield = game.add.tileSprite(0,0,800,600,'starfield');
 		backgroundv = 5;
 
-			player = game.add.sprite.(game.world.centerX, game.world.centerY +200, 'player');
-			game.physics.enable(player, Physics.ARCADE);
+			player = game.add.sprite(game.world.centerX, game.world.centerY +200, 'player');
+			game.physics.enable(player, Phaser.Physics.ARCADE);
+			
+			cursors = game.input.keyboard.createCursorKeys();
+
+			bullets = game.add.group();
+			bullets.enableBody = true;
+
+			bullets.physicsBodyType = Phaser.Physics.ARCADE;
+			bullets.createMultiple(30, 'bullet');
+			bullets.setAll('anchor.x', 0.5);
+			bullets.setAll('anchor.y', 1);
+			bullets.setAll('outOfBoundsKill', true);
+			bullets.setAll('checkWorldBounds', true);
+
+			fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 	},
 
 
 	update: function() {
 
+		player.body.velocity.x = 0;
+
 		spacefield.tilePosition.y += backgroundv;
 
+		if(cursors.left.isDown){
+			player.body.velocity.x = -350;
+		}
+		if(cursors.right.isDown){
+			player.body.velocity.x = 350;
+		}
+
+		if(fireButton.isDown) 
+		{
+			fireBullet();
+		}
+
+
+
+	},
+
+
+
+}
+
+
+function fireBullet(){
+	if(game.time.now > bulletTime){
+		bullet = bullets.getFirstExists(false);
+
+		if (bullet){
+			bullet.reset(player.x + 14, player.y);
+			bullet.body.velocity.y = -400;
+			bulletTime = game.time.now +200;
+		}
 	}
+}
 
-
-
-};
+game.state.add('mainState', mainState);
+game.state.start('mainState');
 
